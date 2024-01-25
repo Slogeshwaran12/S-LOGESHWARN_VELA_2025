567d158fbcfb0c2e8c0f61d08683044e229f8539
Slogeshwaran12 <146526623+Slogeshwaran12@users.noreply.github.com>
2024-01-25 17:05:43 +0530

    Create Question2.py

---
+++ 24.01.24/Question2.py
@@ -0,0 +1,16 @@
+#HAND SHAKE
+
+
+def handshakes(n):
+
+    return (n * (n - 1)) // 2
+
+t = int(input().strip())
+
+
+for _ in range(t)
+    n = int(input().strip())
+    
+
+    result = handshakes(n)
+    print(result)
